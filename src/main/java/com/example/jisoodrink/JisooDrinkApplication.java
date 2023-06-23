ae92bd280dc87bfe53fb85154076218d02038777
hjs9605 <hjs9605@naver.com>
2023-06-23 16:57:02 +0900

    first

---
+++ src/main/java/com/example/jisoodrink/JisooDrinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.jisoodrink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JisooDrinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JisooDrinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jisoodrink/JisooDrinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jisoodrink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JisooDrinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
